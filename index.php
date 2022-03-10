9ba84c0fa74651720b43b19d33238d65ba35b8cf
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-03-10 15:23:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- Page par Jul -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image" href="images/favicon.ico">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Dosis:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="boutique.css">
+    <title>Instagreen Shop</title>
+</head>
+<body>
+    <header>
+        <?php
+        include ("php/header-index.php");
+        ?>
+    </header>
+    <main>
+        <article>
+            <div class="diapo-zone">
+                <img class="diapo" src="images/diapo.gif">
+            </div>
+            <div class="mascotte-zone">
+                <img class="mascotte" src="images/mascotte.png">
+                <p2 class="mascotte-titre"><strong>La marque qui prend de la graine</strong></p2>
+            </div>
+        </article>
+    </main>
+</body>
+</html>
