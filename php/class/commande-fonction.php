4ec6f037d9cd341c057e0db1d4105fb4517de0f5
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-26 21:35:35 +0200

    Add files via upload

---
+++ php/class/commande-fonction.php
@@ -0,0 +1,34 @@
+<!-- Page par Jul -->
+<?php
+// Fonction pour ajouter des produits.
+function ajouterArticles($nom, $description, $prix, $image)
+{
+    if(require('../config.php'))
+    {
+        $request = $access->("INSERT INTO `produit`(`id`, `nom`, `description`, `prix`, `bin`) VALUES ($nom, $description, $prix, $image)");
+        $request->execute(array($nom, $description, $prix, $image));
+        $request->closeCursor();
+    }
+}
+// La fonction pour afficher les produits.
+function afficherProduits()
+{
+    if(require('../config.php'))
+    {
+        $request = $access->prepare("SELECT * FROM `produit` ORDER BY id DESC");
+        $request->execute();
+        $data = $request->fetchAll(PDO::FETCH_OBJ);
+        return $data;
+        $request->closeCursor();
+    }
+}
+// La fonction pour supprimer des articles
+function supprimerArticles($id)
+{
+    if(require('../config.php'))
+    {
+        $request = $access->prepare("DELETE FROM `produit` WHERE id=?");
+        $request->execute(array($id));
+    }
+}
+?>
