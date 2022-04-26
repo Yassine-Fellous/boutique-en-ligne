0fa10e611a3a62e2769006a2e29b0c39abd81ba5
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-26 12:25:03 +0200

    Add files via upload

--- php/connexion.php
+++ php/connexion.php
@@ -1,4 +1,8 @@
-<!-- Page par Jul -->
+<!-- Page par Jul et Yassine -->
+<?php
+session_start(); // On ouvre une session.
+require_once('config.php'); // On appelle la base de données.
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -16,7 +20,7 @@
 <body>
     <main>
         <div class="logo-zone">
-            <img class="logo-co" src="../images/logo.png">
+          <a href="../index.php"><img class="logo-co" src="../images/logo.png"/></a>
         </div>
         <div class="connexion-titre-zone">
             <p1 class="connexion-titre">Connectez-vous</p1>
@@ -24,17 +28,26 @@
         <div class="flex">
           <div class="module-co-zone">
             <div class="formulaire">
-              <form method="POST" action="connexion.php">
+              <form method="POST" action="traitement/tconnexion.php">
                 <label>
-                  <input type="text" name="login" placeholder="Adresse mail ..." required><br /></input>
+                  <input type="text" name="login" placeholder="Adresse mail ..." maxlength="255" required value="<?php if (isset($_SESSION['email'])) {echo $_SESSION['email']; } ?>"><br /></input>
                 </label>
                 <label>
                   <input type="password" name="password" placeholder="Mot de passe ..." required><br /></input>
                 </label>
-                <input class="connexion" type="submit" value="Connexion" name="Connexion"></input>
-                <input class="connexion" type="submit" value="S'inscrire"></input><br />
+                <div class="flex-button">
+                  <input class="connexion" type="submit" value="Connexion" name="Connexion"></input>
+                </div><br />
+                <?php
+                if(isset($_SESSION['fail']))
+                {
+                  echo "$_SESSION[fail]" . "<br>";
+                }
+                ?> <!--Affiche la varibale qui contient l'erreur-->
                 <p5 class="recuperation"> Mot de passe perdu ?<br />
-                 Vous pouvez tenter une <a href="recuperation.php">récupération.</p5></a>
+                Vous pouvez tenter une <a href="recuperation.php">récupération.</p5></a><br />
+                <p5 class="recuperation">Pas encore membre ?<br />
+                Vous pouvez vous <a href="inscription.php">inscrire.</p5></a>
               </form>
             </div>
           </div>
