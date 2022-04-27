05b490bd4e313dd4d7bea2c71887ee0580a1212a
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-27 12:19:36 +0200

    Add files via upload

--- php/class/commande-fonction.php
+++ php/class/commande-fonction.php
@@ -3,32 +3,33 @@
 // Fonction pour ajouter des produits.
 function ajouterArticles($nom, $description, $prix, $image)
 {
-    if(require('../config.php'))
+    if(require('../config.php')) // On appelle la base de données.
     {
-        $request = $access->prepare("INSERT INTO `produit`(`id`, `nom`, `description`, `prix`, `bin`) VALUES ($nom, $description, $prix, $image)");
+        $request = $db->prepare("INSERT INTO `produit`(`id`, `nom`, `description`, `prix`, `bin`) VALUES ($nom, $description, $prix, $image)");
         $request->execute(array($nom, $description, $prix, $image));
-        $request->closeCursor();
+        $request->closeCursor(); // On ferme le curseur.
     }
 }
 // La fonction pour afficher les produits.
 function afficherProduits()
 {
-    if(require('php/config.php'))
+    if(require('php/config.php')) // On appelle la base de données.
     {
-        $request = $access->prepare("SELECT * FROM `produit` ORDER BY id DESC");
+        $request = $db->prepare("SELECT * FROM `produit` ORDER BY id DESC");
         $request->execute();
         $data = $request->fetchAll(PDO::FETCH_OBJ);
         return $data;
-        $request->closeCursor();
+        $request->closeCursor(); // On ferme le curseur.
     }
 }
 // La fonction pour supprimer des articles
 function supprimerArticles($id)
 {
-    if(require('../config.php'))
+    if(require('../config.php')) // On appelle la base de données.
     {
-        $request = $access->prepare("DELETE FROM `produit` WHERE id=?");
+        $request = $db->prepare("DELETE FROM `produit` WHERE id=?");
         $request->execute(array($id));
+        $request->closeCursor(); // On ferme le curseur.
     }
 }
 ?>
