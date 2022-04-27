a4709b7b4eb060d355eebe5dd9f177bdc4795a9e
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-27 12:29:30 +0200

    Add files via upload

--- php/produit.php
+++ php/produit.php
@@ -1,10 +1,16 @@
+<!-- Page par Sofiane et Jul -->
+<?php
+require_once('class/commande-fonction.php'); // On appelle la page de fonctions de commandes.
+$produits=afficherProduits(); // On appelle la classe pour afficher les produits.
+?>
+<?php foreach($produits as $produit) : ?> <!-- Une boucle qui permet d'afficher les produits dans la BDD. -->
 <section class="product_section layout_padding2-top layout_padding-bottom">
  <div class="container">
   <div class="row">
    <div class="col-sm-6 col-lg-4">
     <div class="box">
      <div class="img-box">
-      <img src="images/p1.png" alt="" />
+      <img src="<?= $produit->img ?>"/>
      </div>
      <div class="detail-box">
       <span class="rating">
@@ -14,125 +20,13 @@
        <i class="fa fa-star" aria-hidden="true"></i>
        <i class="fa fa-star-half-o" aria-hidden="true"></i>
       </span>
-      <a href="">
-       OG Kush
-      </a>
+      <a href=""><?= $produit->nom ?></a><br />
+      <h5 href=""><?= $produit->description ?></a>
       <div class="price_box">
-       <h6 class="price_heading"><span>€</span> 10.00</h6>
+       <h6 class="price_heading"><strong><?= $produit->prix ?> €</h6></strong>
       </div>
      </div>
     </div>
    </div>
-   <div class="col-sm-6 col-lg-4">
-    <div class="box">
-     <div class="img-box">
-      <img src="images/p2.png" alt="" />
-     </div>
-     <div class="detail-box">
-      <span class="rating">
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star-half-o" aria-hidden="true"></i>
-      </span>
-      <a href="">
-       Orange Bud
-      </a>
-      <div class="price_box">
-       <h6 class="price_heading"><span>€</span> 10.00</h6>
-      </div>
-     </div>
-    </div>
-   </div>
-   <div class="col-sm-6 col-lg-4">
-    <div class="box">
-     <div class="img-box">
-      <img src="images/p3.png" alt="" />
-     </div>
-     <div class="detail-box">
-      <span class="rating">
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star-half-o" aria-hidden="true"></i>
-      </span>
-      <a href="">
-       Gorilla glue
-      </a>
-      <div class="price_box">
-       <h6 class="price_heading"><span>€</span> 10.00</h6>
-      </div>
-     </div>
-    </div>
-   </div>
-   <div class="col-sm-6 col-lg-4">
-    <div class="box">
-     <div class="img-box">
-      <img src="images/p4.png" alt="" />
-     </div>
-     <div class="detail-box">
-      <span class="rating">
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star-half-o" aria-hidden="true"></i>
-      </span>
-      <a href="">
-       White Widow
-      </a>
-      <div class="price_box">
-       <h6 class="price_heading"><span>€</span> 10.00</h6>
-      </div>
-     </div>
-    </div>
-   </div>
-   <div class="col-sm-6 col-lg-4">
-    <div class="box">
-     <div class="img-box">
-      <img src="images/p5.png" alt="" />
-     </div>
-     <div class="detail-box">
-      <span class="rating">
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star-half-o" aria-hidden="true"></i>
-      </span>
-      <a href="">
-       Banana kush
-      </a>
-      <div class="price_box">
-       <h6 class="price_heading"><span>€</span> 10.00</h6>
-      </div>
-     </div>
-    </div>
-   </div>
-   <div class="col-sm-6 col-lg-4">
-    <div class="box">
-     <div class="img-box">
-      <img src="images/p6.png" alt="" />
-     </div>
-     <div class="detail-box">
-      <span class="rating">
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star" aria-hidden="true"></i>
-       <i class="fa fa-star-half-o" aria-hidden="true"></i>
-      </span>
-      <a href="">
-       Bublble kush
-      </a>
-      <div class="price_box">
-       <h6 class="price_heading"><span>€</span> 10.00</h6>
-      </div>
-     </div>
-    </div>
-   </div>
-  </div>
- </div>
 </section>
+<?php endforeach; ?>
