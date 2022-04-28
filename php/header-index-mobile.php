5ae078f7d893d2f9978444eda377cbab5e44419c
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-29 00:15:11 +0200

    Add files via upload

--- php/header-index-mobile.php
+++ php/header-index-mobile.php
@@ -1,6 +1,5 @@
 <!-- Page par Jul -->
 <!-- Header dynamique -->
-<!-- ?php if(isset($_SESSION['id'])) : ?> Lorsque la personne est connectée, il verra ces sections dans le header. -->
     <img class="logo-header" src="images/logo.png">
     <div class="hamburger-menu">
     <input id="menu__toggle" type="checkbox" />
@@ -10,8 +9,14 @@
 
     <ul class="menu__box">
       <li><a class="menu__item" href="#">Panier</a></li>
-      <li><a class="menu__item" href="produit.php">Produits</a></li>
-      <li><a class="menu__item" href="connexion.php">Connexion</a></li>
+      <?php if(isset($_SESSION['id'])) : ?> <!-- Lorsque la personne est connectée, il verra ces sections dans le header. -->
+        <li><a class="menu__item" href="php/deconnexion.php">Déconnexion</a></li>
+        <?php if($_SESSION['id_droit'] == 1337) : ?> <!-- Seul l'admin verra cette section dans le header. 👮 -->
+          <li><a class="menu__item" href="php/admin.php">Panel d'administration</a></li>
+          <?php endif; ?>
+          <?php else: ?> <!-- Lorsque la personne est déconnectée, il verra ces 2 sections dans le header. -->
+            <li><a class="menu__item" href="php/connexion.php">Connexion</a></li>
+            <?php endif; ?>
       <li><a class="menu__item" href="#">Contact</a></li>
       <li><a class="menu__item" href="https://github.com/Yassine-Fellous/boutique-en-ligne">Code source</a></li>
     </ul>
