83def4cc5fcfb45d923783e37c7146232144535f
Yassine-Fellous <91788542+Yassine-Fellous@users.noreply.github.com>
2022-04-28 10:33:55 +0200

    Add files via upload

---
+++ class/commande-fonction.php
@@ -0,0 +1,38 @@
+<!-- Page par Jul -->
+<?php
+// Fonction pour ajouter des produits.
+function ajouterArticles($nom, $description, $prix, $image)
+{
+    if(require('config.php')) // On appelle la base de données.
+    {echo'ok';
+        $request = $db->prepare("INSERT INTO `produit`(`nom`, `description`, `prix`, `img`) VALUES(?, ?, ?, ?)");
+        echo'ok';
+        $request->execute(array($nom, $description, $prix, $image));
+        echo'ok';
+        $request->closeCursor(); // On ferme le curseur.
+        echo'ok';
+    }
+}
+// La fonction pour afficher les produits.
+function afficherProduits()
+{
+    if(require('../config.php')) // On appelle la base de données.
+    {
+        $request = $db->prepare("SELECT * FROM `produit` ORDER BY id DESC");
+        $request->execute();
+        $data = $request->fetchAll(PDO::FETCH_OBJ);
+        return $data;
+        $request->closeCursor(); // On ferme le curseur.
+    }
+}
+// La fonction pour supprimer des articles
+function supprimerArticles($id)
+{
+    if(require('../config.php')) // On appelle la base de données.
+    {
+        $request = $db->prepare("DELETE FROM `produit` WHERE id=?");
+        $request->execute(array($id));
+        $request->closeCursor(); // On ferme le curseur.
+    }
+}
+?>
