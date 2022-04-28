ee847bfadeb734878f74dfb36caae2d814336906
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-28 16:58:03 +0200

    Add files via upload

--- php/class/moduleco.php
+++ php/class/moduleco.php
@@ -11,8 +11,8 @@ class Modulco{
             $db = new PDO("mysql:host = localhost ;dbname=boutique_en_ligne", 'root', '');
 
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo "ERREUR :" . " " . $e->getMessage();
+        } catch (PDOException $message) {
+            echo "ERREUR :" . " " . $message->getMessage();
         }
 
         $this->bdd = $db;
