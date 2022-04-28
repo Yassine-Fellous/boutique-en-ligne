d7cbd1c6712c92c47e0d986f9744d9bbd1d5be46
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-28 16:52:19 +0200

    Add files via upload

--- php/class/commande-fonction.php
+++ php/class/commande-fonction.php
@@ -4,13 +4,11 @@
 function ajouterArticles($nom, $description, $prix, $image)
 {
     if(require('config.php')) // On appelle la base de données.
-    {echo'ok';
-        $request = $db->prepare("INSERT INTO `produit`(`nom`, `description`, `prix`, `img`) VALUES ('?', '?', '?', '?')");
-        echo'ok';
-        $oui = $request->execute(array($nom, $description, $prix, $image));
-        var_dump($oui);
+    {
+        echo'Le produit à été ajouté.';
+        $request = $db->prepare("INSERT INTO `produit`(`nom`, `description`, `prix`, `img`) VALUES(?, ?, ?, ?)");
+        $request->execute(array($nom, $description, $prix, $image));
         $request->closeCursor(); // On ferme le curseur.
-        echo'ok';
     }
 }
 // La fonction pour afficher les produits.
@@ -25,10 +23,21 @@ function afficherProduits()
         $request->closeCursor(); // On ferme le curseur.
     }
 }
+function afficherProduits2()
+{
+    if(require('config.php')) // On appelle la base de données.
+    {
+        $request = $db->prepare("SELECT * FROM `produit` ORDER BY id DESC");
+        $request->execute();
+        $data = $request->fetchAll(PDO::FETCH_OBJ);
+        return $data;
+        $request->closeCursor(); // On ferme le curseur.
+    }
+}
 // La fonction pour supprimer des articles
 function supprimerArticles($id)
 {
-    if(require('../config.php')) // On appelle la base de données.
+    if(require('config.php')) // On appelle la base de données.
     {
         $request = $db->prepare("DELETE FROM `produit` WHERE id=?");
         $request->execute(array($id));
--- php/class/moduleco.php
+++ php/class/moduleco.php
@@ -1,153 +1,242 @@
 <?php
 session_start();
-class Modulco
-{
+class Modulco{
 
-    private $bdd;
+    private $db;
 
     public function __construct()
     {
         try {
 
-            $bdd = new PDO("mysql:host = localhost ;dbname=boutique_en_ligne", 'root', 'root');
+            $db = new PDO("mysql:host = localhost ;dbname=boutique_en_ligne", 'root', '');
 
-            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "ERREUR :" . " " . $e->getMessage();
         }
 
-        $this->bdd = $bdd;
+        $this->bdd = $db;
     }
 
-    public function register($_nom, $_prenom, $_email, $_adresse, $_password, $_passwordConfirm)
-    {
-
+    public function register($_nom, $_prenom, $_email, $_adresse ,$_password, $_passwordConfirm){
+        
         echo 'ok';
-
-        $bdd = $this->bdd;
-
+        
+        $db = $this->bdd;
+        
         $nom =  htmlspecialchars($_nom);
         $prenom = htmlspecialchars($_prenom);
         $email = htmlspecialchars($_email);
         $adresse = htmlspecialchars($_adresse);
         $password = hash('sha512', $_password);
-
+        
         $_SESSION['nom'] = $_nom;
         $_SESSION['prenom'] = $_prenom;
         $_SESSION['adresse'] = $_adresse;
         $_SESSION['email'] = $_email;
-
+        
         if (!empty($_nom) && !empty($_prenom) && !empty($_email) && !empty($_adresse) && !empty($_password) && !empty($_passwordConfirm)) {
-            echo 'ok';
-            $pasdedoublons = $bdd->prepare("SELECT * FROM user WHERE email = ?");
+        echo'ok';
+            $pasdedoublons = $db->prepare("SELECT * FROM user WHERE email = ?");
             $pasdedoublons->execute(array($email));
             $sidoublon = $pasdedoublons->rowCount();
 
-            if ($sidoublon == 0) {
-                echo 'ok';
+            if ($sidoublon == 0) { 
+            echo'ok';
                 if ($_passwordConfirm == $_password) {
-                    echo 'ok';
-                    $insertinto = $bdd->prepare("INSERT INTO user(nom, prenom, email, adresse, password, id_droit) VALUES(?, ?, ?, ?, ?, 1)");
+                echo'ok';
+                    $insertinto = $db->prepare("INSERT INTO user(nom, prenom, email, adresse, password) VALUES(?, ?, ?, ?, ?)");
                     $insertinto->execute(array($nom, $prenom, $email, $adresse, $password));
                     unset($_SESSION);
                     header('Location:../connexion.php');
                 } else {
-                    echo 'no';
+                    echo'no';
                     $_SESSION['fail'] =  '<font color="red">les passwords ne concordent pas !!!</font>';
-                    header('Location:../inscription.php');
+                    header('Location:finscription.php');
                 }
             } else {
                 echo 'no';
                 $_SESSION['fail'] = '<font color="red">Cette email est dejà utilisé !</font>';
-                header('Location:../inscription.php');
+                header('Location:finscription.php');
             }
         } else {
-            echo 'no';
+            echo'no';
             $_SESSION['fail'] = '<font color="red">Il manque des champs !</font>';
-            header('Location:../inscription.php');
+            header('Location:finscription.php');
         }
     }
-
-    public function connect($_email, $_password)
-    {
-
-        $bdd = $this->bdd;
-
+    
+    public function connect($_email, $_password){
+        
+        $db = $this->bdd;
+        
         $login = htmlspecialchars($_email);
         $password = hash('sha512', $_password);
-
-        if (!empty($login) && !empty($password)) {
-
-            $request = $bdd->prepare("SELECT * FROM user WHERE email = ? && password = ?");
+        
+        if(!empty($login) && !empty($password)){
+            
+            $request = $db->prepare("SELECT * FROM user WHERE email = ? && password = ?");
             $request->execute(array($login, $password));
             $userexist = $request->rowCount();
 
-            if ($userexist == 1) {
+            if($userexist == 1){
                 $userinfo = $request->fetch();
                 unset($_SESSION['fail']);
                 $_SESSION['id'] = $userinfo['id'];
                 $_SESSION['email'] = $userinfo['email'];
-                header('Location:/./boutique-en-ligne/index.php');
-            } else {
-                $_SESSION['fail'] = '<font color="red">Login inexsistant ou Password incorrect !</font>';
+                $_SESSION['id_droit'] = $userinfo['id_droit'];
+                header('Location:../../index.php');
+                }
+                
+                else{
+                    $_SESSION['fail']= '<font color="red">Login inexsistant ou Password incorrect !</font>';
+                    header('Location:../connexion.php');
+                }
+            }
+            else{
+                $_SESSION['fail'] = '<font color="red"> Il manque des champs !</font>';
                 header('Location:../connexion.php');
             }
-        } else {
-            $_SESSION['fail'] = '<font color="red"> Il manque des champs !</font>';
-            header('Location:../connexion.php');
-        }
     }
 
 
-    //tout ce qui est en bas en commenttaire c'est ce qui reste a programmer
+     //tout les header locationt d'en bas doivent etre modifier
 
 
-    //
-    //public function update($_login, $_password, $_passwordConfirm){
-    //    $_bdd = new PDO("mysql:host=localhost;dbname=livreor", 'root', '' );
-    //
-    //    $requser = $_bdd->prepare('SELECT * FROM utilisateurs WHERE id = ?');
-    //    $requser->execute(array($_SESSION['id']));
-    //    $userinfo = $requser->fetch();
-    //    
-    //    if(isset($_login) && !empty($_login) && $_login != $userinfo['login']){
-    //        
-    //        $newlogin = htmlspecialchars($_login);
-    //        $insertlogin = $_bdd->prepare("UPDATE utilisateurs SET login = ? WHERE id = ?");
-    //        $insertlogin->execute(array($newlogin, $_SESSION['id']));
-    //    
-    //        if(isset($_password) && !empty($_password) && isset($_passwordConfirm) && !empty($_passwordConfirm)) {
-    //
-    //            $newpassword = hash('sha512', $_password);
-    //            
-    //            if($_password == $_passwordConfirm){
-    //                
-    //                $insertlogin = $_bdd->prepare("UPDATE utilisateurs SET password = ? WHERE id = ?");
-    //                $insertlogin->execute(array($newpassword, $_SESSION['id']));
-    //                unset($_SESSION['fail']);
-    //                header('Location:../html/connexion.php');
-    //            }
-    //            else{
-    //                $_SESSION['fail'] = '<font color="red">les passwords ne concordent pas !!!</font>';
-    //                header('Location:../html/profil.php');
-    //            }
-    //        }
-    //        else{
-    //            $_SESSION['fail'] = '<font color="red"> Tout les champs doivent être compléte !!!</font>';
-    //            header('Location:../html/profil.php');
-    //        }
-    //    }
-    //    else{
-    //        $_SESSION['fail'] = '<font color="red">Le Login doit etre different de l\'ancien !!!</font>';
-    //        header('Location:../html/profil.php');
-    //    }
-    //} 
-    //
-    //public function disconnect(){
-    //    
-    //    session_start();
-    //    $_SESSION = array();
-    //    session_destroy();
-    //    header("Location:../index.php");
-    //}
+
+    public function updatenom($_nom){
+        $db = $this->bdd;
+
+        $requser = $db->prepare('SELECT * FROM user WHERE id = ?');
+        $requser->execute(array($_SESSION['id']));
+        $userinfo = $requser->fetch();
+        
+        if(isset($_nom) && !empty($_nom)){
+
+            if($_nom != $userinfo['nom']){
+            
+                $newnom = htmlspecialchars($_nom);
+                $insertnom = $db->prepare("UPDATE user SET login = ? WHERE id = ?");
+                $insertnom->execute(array($newnom, $_SESSION['id']));
+            }
+            else{
+                $_SESSION['fail'] = '<font color="red">les champs doivent être complété !!!</font>';
+                header('Location:../html/profil.php');
+            }
+        }
+        else{
+            $_SESSION['fail'] = '<font color="red">L\'adresse doit etre differente de l\'ancienne !!!</font>';
+            header('Location:../html/profil.php');
+        }
+    }
+
+    public function updateprenom($_prenom){
+        $db = $this->bdd;
+
+        $requser = $db->prepare('SELECT * FROM user WHERE id = ?');
+        $requser->execute(array($_SESSION['id']));
+        $userinfo = $requser->fetch();
+        
+        if(isset($_prenom) && !empty($_prenom) ){
+            
+            if($_prenom != $userinfo['prenom']){
+
+                $newprenom = htmlspecialchars($_prenom);
+                $insertprenom = $db->prepare("UPDATE user SET prenom = ? WHERE id = ?");
+                $insertprenom->execute(array($newprenom, $_SESSION['id']));
+            }
+            else{
+                $_SESSION['fail'] = '<font color="red">Le prenom doit etre differente de l\'ancienne !!!</font>';
+                header('Location:../html/profil.php');
+            }
+        }
+        else{
+            $_SESSION['fail'] = '<font color="red">les champs doivent être complété !!!</font>';
+            header('Location:../html/profil.php');
+        }
+    }
+
+    public function updateemail($_email){
+        $db = $this->bdd;
+
+        $requser = $db->prepare('SELECT * FROM user WHERE id = ?');
+        $requser->execute(array($_SESSION['id']));
+        $userinfo = $requser->fetch();
+        
+        if(isset($_email) && !empty($_email)){
+            
+            if($_email != $userinfo['email']){
+                $newemail = htmlspecialchars($_email);
+                $insertemail = $db->prepare("UPDATE user SET login = ? WHERE id = ?");
+                $insertemail->execute(array($newemail, $_SESSION['id']));
+            }
+            else{
+                $_SESSION['fail'] = '<font color="red">L\'email doit etre differente de l\'ancien !!!</font>';
+                header('Location:../html/profil.php');
+            }
+        }
+        else{
+            $_SESSION['fail'] = '<font color="red">les champs doivent être complété !!!</font>';
+            header('Location:../html/profil.php');
+        }
+    }
+
+    public function updateadresse($_adresse){
+        $db = $this->bdd;
+
+        $requser = $db->prepare('SELECT * FROM user WHERE id = ?');
+        $requser->execute(array($_SESSION['id']));
+        $userinfo = $requser->fetch();
+        
+        if(isset($_adresse) && !empty($_adresse)){
+
+            if($_adresse != $userinfo['adresse']){
+            
+                $newadresse = htmlspecialchars($_adresse);
+                $insertadresse = $db->prepare("UPDATE user SET login = ? WHERE id = ?");
+                $insertadresse->execute(array($newadresse, $_SESSION['id']));
+            }
+        }
+        else{
+            $_SESSION['fail'] = '<font color="red">L\'adresse doit etre differente de l\'ancienne !!!</font>';
+            header('Location:../html/profil.php');
+        }
+    }
+
+    public function updatepassword($_password, $_passwordConfirm){
+
+        $db = $this->bdd;
+        
+        $requser = $db->prepare('SELECT * FROM user WHERE id = ?');
+        $requser->execute(array($_SESSION['id']));
+        $userinfo = $requser->fetch();
+        
+        if(isset($_password) && !empty($_password) && isset($_passwordConfirm) && !empty($_passwordConfirm)) {
+
+            $newpassword = hash('sha512', $_password);
+            
+            if($_password == $_passwordConfirm){
+
+                if($newpassword != $userinfo['password']){
+                
+                    $insertlogin = $db->prepare("UPDATE user SET password = ? WHERE id = ?");
+                    $insertlogin->execute(array($newpassword, $_SESSION['id']));
+                    unset($_SESSION['fail']);
+                    header('Location:../html/connexion.php');
+                }
+                else{
+                    $_SESSION['fail'] = '<font color="red">le mot de passe ne doit pas etre identique au précedent !!!</font>';
+                    header('Location:../html/profil.php');
+                }
+            }
+            else{
+                $_SESSION['fail'] = '<font color="red">les mot de passe ne sont pas identique !!!</font>';
+                header('Location:../html/profil.php');
+            }
+        }
+        else{
+            $_SESSION['fail'] = '<font color="red">les champs doivent être complété !!!</font>';
+            header('Location:../html/profil.php');
+        }
+    }
 }
