bcf21e0b4af0e41536614b064f5d6205e737f755
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-04-29 16:13:04 +0200

    Delete controller directory

--- controller/tconnexion.php
+++
@@ -1,8 +0,0 @@
-<!-- Page par Yassine -->
-<?php
-require '../class/moduleco.php';
-if (isset($_POST['submit'])) {
-    $objet = new Modulco;
-    $objet->connect($_POST['email'], $_POST['password']);
-}
-?>
