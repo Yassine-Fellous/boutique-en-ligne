da5c9e4124e88cc2202ce9687d5ef08d3ab15ff5
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-01 16:02:16 +0200

    Add files via upload

--- php/panier.php
+++ php/panier.php
@@ -16,7 +16,8 @@ $produits = $db->query('SELECT * FROM `produit` ORDER BY id DESC');
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Shippori+Antique+B1&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="../css/boutique.css"/>
     <link rel="stylesheet" type="text/css" href="../css/responsive.css"/>
     <link rel="stylesheet" type="text/css" href="../css/style.css"/>
@@ -71,25 +72,17 @@ $produits = $db->query('SELECT * FROM `produit` ORDER BY id DESC');
                                  </div>
                               </div>
                            </div>
-                           <div class="card mb-3">
-                              <div class="card-body">
-                                 <div class="d-flex justify-content-between">
-                                    <div class="d-flex flex-row align-items-center">
-                                       <div>
-                          <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img2.webp"
-                            class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
-                        </div>
+                           
                         <?php endforeach; ?>
               <div class="col-lg-5">
 
                 <div class="card bg-primary text-white rounded-3">
                   <div class="card-body">
                     <div class="d-flex justify-content-between align-items-center mb-4">
-                      <h5 class="mb-0">Card details</h5>
+                      <h5 class="mb-0">Détails de la carte</h5>
                     </div>
 
-                    <p class="small mb-2">Card type</p>
+                    <p class="small mb-2">Type de cartes acceptés :</p>
                     <a href="#!" type="submit" class="text-white"><i
                         class="fab fa-cc-mastercard fa-2x me-2"></i></a>
                     <a href="#!" type="submit" class="text-white"><i
@@ -101,21 +94,21 @@ $produits = $db->query('SELECT * FROM `produit` ORDER BY id DESC');
                     <form class="mt-4">
                       <div class="form-outline form-white mb-4">
                         <input type="text" id="typeName" class="form-control form-control-lg" siez="17"
-                          placeholder="Cardholder's Name" />
-                        <label class="form-label" for="typeName">Cardholder's Name</label>
+                          placeholder="Elon Musk" />
+                        <label class="form-label" for="typeName">Nom sur la carte</label>
                       </div>
 
                       <div class="form-outline form-white mb-4">
                         <input type="text" id="typeText" class="form-control form-control-lg" siez="17"
                           placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" />
-                        <label class="form-label" for="typeText">Card Number</label>
+                        <label class="form-label" for="typeText">Numéro de carte bancaire</label>
                       </div>
 
                       <div class="row mb-4">
                         <div class="col-md-6">
                           <div class="form-outline form-white">
                             <input type="text" id="typeExp" class="form-control form-control-lg"
-                              placeholder="MM/YYYY" size="7" id="exp" minlength="7" maxlength="7" />
+                              placeholder="MM/AA" size="7" id="exp" minlength="7" maxlength="7" />
                             <label class="form-label" for="typeExp">Expiration</label>
                           </div>
                         </div>
@@ -123,7 +116,7 @@ $produits = $db->query('SELECT * FROM `produit` ORDER BY id DESC');
                           <div class="form-outline form-white">
                             <input type="password" id="typeText" class="form-control form-control-lg"
                               placeholder="&#9679;&#9679;&#9679;" size="1" minlength="3" maxlength="3" />
-                            <label class="form-label" for="typeText">Cvv</label>
+                            <label class="form-label" for="typeText">CVV</label>
                           </div>
                         </div>
                       </div>
