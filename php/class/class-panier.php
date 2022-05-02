f05ab9c5498eb7d14d182fd0e09d8cd26080738a
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-02 13:59:43 +0200

    Add files via upload

--- php/class/class-panier.php
+++ php/class/class-panier.php
@@ -2,7 +2,8 @@
 <?php
 class panier
 {
-    function __construct()
+    private $db;
+    public function __construct($db)
     {
         if(!isset($_SESSION))
         {
@@ -12,10 +13,54 @@ class panier
         {
             $_SESSION['panier'] = array();
         }
+        $this->db = $db;
+        if(isset($_GET['supprimer-produit']))
+        {
+            $this->supprimerProduitDuPanier($_GET['supprimer-produit']);
+        }
+        if(isset($_POST['panier']['quantity']))
+        {
+            $this->quantity();
+        }
+    }
+    public function quantity()
+    {
+        foreach($_SESSION['panier'] as $idProduit => $quantity)
+        {
+            if(isset($_POST['panier']['quantity'][$idProduit]))
+            {
+                $_SESSION['panier'][$idProduit] = $_POST['panier']['quantity'][$idProduit];
+            }
+        }
+    }
+    public function prixTotal()
+    {
+        $prixTotal = 0;
+        $idProduits = array_keys($_SESSION['panier']);
+        if(empty($idProduits))
+        {
+            $produits = array();
+        }
+        else
+        {
+            $produits = $this->db->query('SELECT * FROM `produit` WHERE id IN ('.implode(',',$idProduits).')');
+        }
+        foreach($produits as $produit)
+        {
+            $prixTotal += $produit->prix * $_SESSION['panier'][$produit->id];
+        }
+        return $prixTotal;
     }
     public function ajouterProduitAuPanier($idProduit)
     {
-        $_SESSION['panier'][$idProduit] = 1;
+        if(isset($_SESSION['panier'][$idProduit]))
+        {
+            $_SESSION['panier'][$idProduit]++;
+        }
+        else
+        {
+            $_SESSION['panier'][$idProduit] = 1;
+        }
     }
     public function supprimerProduitDuPanier($idProduit)
     {
