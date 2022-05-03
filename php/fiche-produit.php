62d6e2954ea28e2e8e324e033c0967f748f433f3
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-03 13:07:36 +0200

    Add files via upload

--- php/fiche-produit.php
+++ php/fiche-produit.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 require_once('config.php'); // On appelle la base de données.
-include('class/autres-fonctions.php');
 $db = new bdd();
-$id = $_SESSION['id-produit'];
+$idProduit=$_GET['id']; // Il va récupérer l'utilisateur sélectionné.
+$idProduit = $db->query("SELECT * FROM `produit` WHERE id = '$idProduit'"); // Je sélectionne les membres.
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -28,10 +28,36 @@ $id = $_SESSION['id-produit'];
             <p1 class="connexion-titre">Fiche produit</p1>
         </div>
         <section>
+        <?php foreach($idProduit as $produit) : ?> <!-- Une boucle qui permet d'afficher les produits dans la BDD. -->
            <div class="container">
-              <div class="row">
-              <?php 
-              $user = new Pageproduit;
-              $user->ficheProduit($id); 
-              ?>
-            
+           <div class="col-sm-6 col-lg-4">
+               <div class="box">
+                  <div class="img-box">
+                     <img src="<?= $produit->img ?>"/>
+                  </div>
+                  <div class="detail-box">
+                     <span class="rating">
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                        <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                     </span>
+                     <a href=""><?= $produit->nom ?></a><br /> <!-- Via cet argument, il va afficher la liste des noms des produits présents dans la BDD. -->
+                     <h5 href=""><?= substr($produit->description, 0, 50); ?>...</a> <!-- Via cet argument, il va afficher la liste des descriptions des produits présents dans la BDD. La limite de caractère est appliquée à 50 caractères. -->
+                     <div class="price_box">
+                        <h6 class="price_heading"><strong><?= number_format($produit->prix,2,',',' '); ?> €</h6></strong>
+                     </div>
+                     <div class="flex-button">
+                        <a href="php/ajouter-au-panier.php?id=<?= $produit->id; ?>"><input class="connexion" type="submit" value="Ajouter au panier" name="panier"></input></a>
+                     </div><br />
+                     <div class="flex-button">
+                        <a href="php/fiche-produit.php?id=<?= $produit->id; ?>"><input class="connexion" type="submit" value="Voir le produit" name="submit"></input></a>
+                     </div><br />
+                  </div>
+               </div>
+            </div>
+         <?php endforeach; ?> <!-- Fin de la boucle -->
+      </div>
+   </div>
+</section>
