87709555d150d09a6e58f76d8e6cca587a013386
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-05-03 15:27:58 +0200

    Add files via upload

--- php/fiche-produit.php
+++ php/fiche-produit.php
@@ -1,9 +1,10 @@
+<!-- Page par Jul -->
 <?php
 session_start();
 require_once('config.php'); // On appelle la base de données.
 $db = new bdd();
-$idProduit=$_GET['id']; // Il va récupérer l'utilisateur sélectionné.
-$idProduit = $db->query("SELECT * FROM `produit` WHERE id = '$idProduit'"); // Je sélectionne les membres.
+$idProduit = $_GET['id']; // Il va récupérer l'ID du produit sélectionné.
+$idProduit = $db->query("SELECT * FROM `produit` WHERE id = '$idProduit'"); // J'affiche tous les produits, mais seulement celui qu'on a choisi, donc par l'ID.
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -16,6 +17,7 @@ $idProduit = $db->query("SELECT * FROM `produit` WHERE id = '$idProduit'"); // J
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Shippori+Antique+B1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../css/boutique.css"/>
     <title>Fiche du produit - Instagreen Shop</title>
 </head>
@@ -27,37 +29,16 @@ $idProduit = $db->query("SELECT * FROM `produit` WHERE id = '$idProduit'"); // J
         <div class="connexion-titre-zone">
             <p1 class="connexion-titre">Fiche produit</p1>
         </div>
-        <section>
-        <?php foreach($idProduit as $produit) : ?> <!-- Une boucle qui permet d'afficher les produits dans la BDD. -->
-           <div class="container">
-           <div class="col-sm-6 col-lg-4">
-               <div class="box">
-                  <div class="img-box">
-                     <img src="<?= $produit->img ?>"/>
-                  </div>
-                  <div class="detail-box">
-                     <span class="rating">
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                        <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                     </span>
-                     <a href=""><?= $produit->nom ?></a><br /> <!-- Via cet argument, il va afficher la liste des noms des produits présents dans la BDD. -->
-                     <h5 href=""><?= substr($produit->description, 0, 50); ?>...</a> <!-- Via cet argument, il va afficher la liste des descriptions des produits présents dans la BDD. La limite de caractère est appliquée à 50 caractères. -->
-                     <div class="price_box">
-                        <h6 class="price_heading"><strong><?= number_format($produit->prix,2,',',' '); ?> €</h6></strong>
-                     </div>
-                     <div class="flex-button">
-                        <a href="php/ajouter-au-panier.php?id=<?= $produit->id; ?>"><input class="connexion" type="submit" value="Ajouter au panier" name="panier"></input></a>
-                     </div><br />
-                     <div class="flex-button">
-                        <a href="php/fiche-produit.php?id=<?= $produit->id; ?>"><input class="connexion" type="submit" value="Voir le produit" name="submit"></input></a>
-                     </div><br />
-                  </div>
-               </div>
+            <div class="fiche-produit-zone">
+                <div class="fiche-produit">
+                    <?php foreach($idProduit as $produit) : ?> <!-- Une boucle qui permet d'afficher les produits dans la BDD. -->
+                        <h2 class="nom-fiche-produit"><?= $produit->nom ?></a><br /> <!-- Via cet argument, il va afficher la liste des noms des produits présents dans la BDD. -->
+                        <img class="image-produit" src="../<?= $produit->img ?>"/>
+                        <h4 class="description-produit"><?= $produit->description ?>...</h4><br /> <!-- Via cet argument, il va afficher la liste des descriptions des produits présents dans la BDD. -->
+                        <h6 class="prix-fiche-produit"><strong><?= number_format($produit->prix,2,',',' '); ?> €</h6></strong>
+                        <div class="flex-button">
+                            <a href="ajouter-au-panier.php?id=<?= $produit->id; ?>"><input class="connexion" type="submit" value="Ajouter au panier" name="panier"></input></a>
+                        </div><br />
+                    <?php endforeach; ?> <!-- Fin de la boucle -->
+                </div>
             </div>
-         <?php endforeach; ?> <!-- Fin de la boucle -->
-      </div>
-   </div>
-</section>
