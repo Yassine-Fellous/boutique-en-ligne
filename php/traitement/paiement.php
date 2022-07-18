350bfdc0ac785277b87feb7a4c6e6640761ec606
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-07-18 21:26:37 +0200

    Add files via upload

--- php/traitement/paiement.php
+++ php/traitement/paiement.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // Section pour les erreurs (logs)
 error_reporting(-1); // Montre tous les erreurs
 ini_set("display_errors", "1");
@@ -16,18 +17,15 @@ $db = new bdd(); // On appelle la class bdd.
 $panier = new panier($db);
 $produits = $db->query('SELECT * FROM `produit` ORDER BY id DESC');
 // Pour Stripe
-if(isset($_POST['prix']) && !empty($_POST['prix']))
+require_once('../../vendor/autoload.php');
+$prix = $panier->prixTotal();
+if($prix <=0)
 {
-    require_once('../../vendor/autoload.php');
-    $prix = (float)$_POST['prix'];
-    // Instance Stripe
-    \Stripe\Stripe::setApiKey('sk_test_51KUAgRJcdqGu57WBKfKR5bC4PWbrSqCdFhOz82WDlBSpBaQedtbOcipbzIMunm122C0gwuX1psXLIF9TiFa1PQYP00vRA06fQA'); // Clé secrète
-    $intention = \Stripe\PaymentIntent::create(['amount' => $prix*100, 'currency' => 'eur']);
+  header("../erreur-paiement.php");
 }
-else
-{
-    header('location: ../../index.php');
-}
+// Instance Stripe
+\Stripe\Stripe::setApiKey('sk_test_51KUAgRJcdqGu57WBKfKR5bC4PWbrSqCdFhOz82WDlBSpBaQedtbOcipbzIMunm122C0gwuX1psXLIF9TiFa1PQYP00vRA06fQA'); // Clé secrète
+$intention = \Stripe\PaymentIntent::create(['amount' => $prix*100, 'currency' => 'eur']);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -48,42 +46,14 @@ else
             <div class="d-flex justify-content-between align-items-center mb-4">
               <h5 class="mb-0">Détails de la carte</h5>
             </div>
-            <p class="small mb-2">Type de cartes acceptés :</p>
-            <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-mastercard fa-2x me-2"></i></a>
-            <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-visa fa-2x me-2"></i></a>
-            <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-amex fa-2x me-2"></i></a>
-            <a href="#!" type="submit" class="text-white"><i class="fab fa-cc-paypal fa-2x"></i></a>
-            <form class="mt-4">
+            <form method="POST" class="mt-4">
               <div class="form-outline form-white mb-4">
-                <form action="traitement/paiement.php" method="POST" id="formulaire_paiement">
-                  <input type="text" id="typeName" class="form-control form-control-lg" siez="17" placeholder="Elon Musk" required/>
-                  <label class="form-label" for="typeName">Nom sur la carte</label>
+                <form action="traitement/paiement.php" method="POST">
+                  <input type="text" id="cardholder-name" class="form-control form-control-lg" placeholder="Elon Musk" required/>
+                  <label class="form-label" for="cardholder-name">Titulaire de la carte</label>
                 </div>
-                <div class="form-outline form-white mb-4">
-                  <input type="text" id="typeText" class="form-control form-control-lg" siez="17" placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" data-stripe="number" required/>
-                  <label class="form-label" for="typeText">Numéro de carte bancaire</label>
-                </div>
-                <div class="row mb-4">
-                  <div class="col-md-6">
-                    <div class="form-outline form-white">
-                      <input type="text" id="typeYear" class="form-control form-control-lg" placeholder="MM" size="7" id="exp" minlength="2" maxlength="2" data-stripe="exp_month" required/>
-                      <label class="form-label" for="typeExp">Mois d'expiration</label>
-                    </div>
-                  </div>
-                  <div class="row mb-4">
-                    <div class="col-md-6">
-                      <div class="form-outline form-white">
-                        <input type="text" id="typeExp" class="form-control form-control-lg" placeholder="202x" size="7" id="exp" minlength="4" maxlength="4" data-stripe="exp_year" required/>
-                        <label class="form-label" for="typeExp">Année d'expiration</label>
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-outline form-white">
-                        <input type="password" id="typeText" class="form-control form-control-lg" placeholder="&#9679;&#9679;&#9679;" size="1" minlength="3" maxlength="3" data-stripe="cvc" required/>
-                        <label class="form-label" for="typeText">CVV</label>
-                      </div>
-                    </div>
-                  </div>
+                <div id="card-elements"></div> <!-- Formulaire des infos de la carte -->
+                <div id="card-errors" role="alert"></div> <!-- Erreurs liés à la carte -->
                   <hr class="my-4">
                   <div class="d-flex justify-content-between mb-4">
                     <p class="mb-2">Total</p>
@@ -91,7 +61,7 @@ else
                   </div>
                   <button name="prix" id="prix" class="btn btn-info btn-block btn-lg">
                     <div class="d-flex justify-content-between">
-                      <span>Payer<i class="fas fa-long-arrow-alt-right"></i></span>
+                      <button id="card-button" type="button" data-secret="<?= $intention['client_secret'] ?>">Payez</button><span>Payer<i class="fas fa-long-arrow-alt-right"></i></span>
                     </div>
                   </button>
                 </form>
